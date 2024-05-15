af02a96cb3d24523adb955ee6996e671eeeaf0dd
Thaw Zin Soe <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2024-05-15 14:58:57 +0630

    Word Replacement

---
+++ Python Projects/Word Replacement Program/main.py
@@ -0,0 +1,8 @@
+text = "Python is a versatile programming language that is widely used for various applications, including web development, data analysis, artificial intelligence, and automation. Its simple syntax and readability make it an excellent choice for beginners, while its powerful libraries and frameworks provide advanced capabilities for experienced developers. Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. It has a vibrant community and extensive documentation, which are valuable resources for learning and troubleshooting. With its growing popularity and diverse applications, Python is an essential language for anyone looking to pursue a career in technology."
+print(text)
+old = "Python"
+new = "PHP"
+print("#########################")
+def replace_function(text, old, new):
+    print(text.replace(old, new))
+replace_function(text,old,new)
